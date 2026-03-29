195d958ae9d7868d447d95ee41f735fb14f8eac6
Warwick Molloy <wazzam@lokeldigital.com>
2026-03-30 04:11:22 +1100

    Version string is taken from the one function that is used in the Station config version.

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,10 @@
 
+/** Returns the fixed version string set by the build automation from
+ *  the source project-version text file.
+ * 
+ * @returns version string that is set during build to match text
+ *          in the `project-version` file.
+ */
 export function atp_version() : string {
   return process.env.ATP_VERSION || "0.0.0-dev";
 }
