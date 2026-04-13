274886e2417859a9421ff5a14c0d8a3231370881
Warwick Molloy <wmolloy@gmail.com>
2026-04-14 07:16:14 +1000

    Example skills pack works as intended - installing 2 skills.

--- example-skills-pack/pack.sh
+++ example-skills-pack/pack.sh
@@ -7,12 +7,16 @@ atp create package skeleton
 atp package name "Example skills pack"
 atp package developer "Warwick Molloy"
 atp package license "Apache 2.0"
-atp package version 0.1.0
+atp package version 0.1.1
 atp package copyright "Warwick Molloy 2026"
 
 atp package newpart skill
 atp package part 1 usage "Writing markdown documents" 
 atp package part 1 bundle add doc-writing --skip-exec
+
+atp package newpart skill
+atp package part 2 usage "Ask the agent to write notes." 
+atp package part 2 bundle add note-taking --skip-exec
 
 atp package summary
 atp catalog add package
